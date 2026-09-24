cd11f66d9e9d11b2a3d5f343ef228bbd930c9e70
Baohung Nguyen <skystream006@gmail.com>
2026-09-23 22:24:16 -0400

    update docker script

--- update_docker_gpu.ps1
+++ update_docker_gpu.ps1
@@ -1,1 +1,2 @@
 git pull && docker compose -f compose.yaml -f compose.gpu.yaml up --build -d
+docker compose -f compose.yaml -f compose.gpu.yaml exec -u root sstranscriber sh -c '/sbin/ldconfig /usr/lib/wsl/drivers/*'
